ccb9a438ccbd122821fe7c6018e98db8a5f15a8b
Arjayy007 <116645261+Arjayy007@users.noreply.github.com>
2025-09-15 17:46:41 +0800

    chore: scaffold project with react and vite

---
+++ frontend/src/pages/Focal/Landing/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+
+
+export function Landing() {
+  const navigate = useNavigate()
+  return (
+    <div className="flex flex-col justify-center items-center h-screen relative">
+      <h1>Focal Landing Page</h1>
+      <Button onClick={() => navigate('/login-focal')} >Login</Button>
+    </div>
+  )
+}
