c3ee9992c09464652a2a4c5de06a633a9f1af475
Xine003 <108078507+Xine003@users.noreply.github.com>
2025-09-23 20:22:00 +0800

    Added the Image for the Focal Person and Alternative FP

--- backend/src/routes/focalPersonRoutes.js
+++ backend/src/routes/focalPersonRoutes.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const router = express.Router();
+
 const {
     createFocalPerson,
     getFocalPerson,
     getFocalPersons,
-    updateFocalPerson
+    updateFocalPerson,
+    updateFocalPhotos,
+    getFocalPhoto,
+    getAlternativeFocalPhoto
 } = require("../controllers/focalPersonController");
+
+const { uploadFocalPhotos } = require("../middleware/uploadFocalPhotos");
 
 // CRUD
 router.post("/", createFocalPerson);
 router.get("/", getFocalPersons);
 router.get("/:id", getFocalPerson);
+router.get("/:id/photo", getFocalPhoto);
+router.get("/:id/altPhoto", getAlternativeFocalPhoto);
 router.put("/:id", updateFocalPerson);
+router.put("/:id/photos", uploadFocalPhotos, updateFocalPhotos);
+
 
 module.exports = router;
