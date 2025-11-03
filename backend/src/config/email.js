d9c3fed90a309cbcb0292e83a0cb35b602180c14
jing388 <uy.gwynethfabul@gmail.com>
2025-11-03 21:06:18 +0800

    try again for hosting

--- backend/src/config/email.js
+++ backend/src/config/email.js
@@ -1,9 +1,10 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-
 const transporter = nodemailer.createTransport({
   service: "gmail", // or "hotmail", "yahoo", "outlook"
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
