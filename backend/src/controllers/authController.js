76224f32f1263d31873efb8558526c6bbef51f75
Xine003 <108078507+Xine003@users.noreply.github.com>
2025-12-09 00:32:56 +0800

    Changes in User in AuthController

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -4,6 +4,7 @@ const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 const { AppDataSource } = require("../config/dataSource");
 const SibApiV3Sdk = require("sib-api-v3-sdk");
+require("dotenv").config();
 
 const client = SibApiV3Sdk.ApiClient.instance;
 const apiKey = client.authentications["api-key"];
@@ -158,7 +159,7 @@ const focalLogin = async (req, res) => {
 
     // Send OTP using Brevo
     try {
-      const sender = { email: "rielkai01@gmail.com", name: "ResQWave" };
+      const sender = { email: process.env.EMAIL_USER, name: "ResQWave Team" };
       const receivers = [{ email: focal.email }];
 
       await tranEmailApi.sendTransacEmail({
@@ -423,7 +424,7 @@ const adminDispatcherLogin = async (req, res) => {
 
     // Send email
     try {
-      const sender = { email: "resqwaveinfo@gmail.com", name: "ResQWave Team" }; // or your verified Brevo sender
+      const sender = { email: process.env.EMAIL_USER, name: "ResQWave Team" }; // or your verified Brevo sender
       const receivers = [{ email: recipientEmail }];
 
       await tranEmailApi.sendTransacEmail({
@@ -743,7 +744,7 @@ const resendFocalLoginCode = async (req, res) => {
 
     // Send email
     try {
-      const sender = { email: "resqwaveinfo@gmail.com", name: "ResQWave Team" };
+      const sender = { email: process.env.EMAIL_USER, name: "ResQWave Team" };
       const receivers = [{ email: focal.email }];
 
       await tranEmailApi.sendTransacEmail({
@@ -875,7 +876,7 @@ const resendAdminDispatcherCode = async (req, res) => {
 
     // Send Email
     try {
-      const sender = { email: "resqwaveinfo@gmail.com", name: "ResQWave Team" }; // must be verified in Brevo
+      const sender = { email: process.env.EMAIL_USER, name: "ResQWave Team" }; // must be verified in Brevo
       const receivers = [{ email: recipientEmail }];
 
       await tranEmailApi.sendTransacEmail({
